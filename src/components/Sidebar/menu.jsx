65b5b7bdd29800c076d81f805e33d88bccfd2f40
Sergio C. Orozco Torres <scot3004@gmail.com>
2021-03-30 23:01:27 -0500

    Sidebar migrated to theme-ui

--- src/components/Sidebar/menu.jsx
+++ src/components/Sidebar/menu.jsx
@@ -1,46 +1,46 @@
-import styled from '@emotion/styled'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 
-const StyledMenu = styled.nav`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background: ${(props) => props.theme.colors.sidebar.background};
-  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
-  height: 100vh;
-  text-align: left;
-  padding: 1rem;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
-  overflow: auto;
+const Menu = ({children, open}) => (
+  <nav
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      background: (theme) => theme.colors.sidebar.background,
+      transform: open ? 'translateX(0)' : 'translateX(-100%)',
+      height: '100vh',
+      textAlign: 'left',
+      padding: '1rem',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      zIndex: '100',
+      overflow: 'auto',
+      transition: 'transform 0.3s ease-in-out',
+      width: '100%',
+      '@media (min-width: 830px)': {
+        width: (theme) => theme.sizes.sidebar,
+      },
+      'a': {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        color: (theme) => theme.colors.sidebar.color,
+        fontFamily:'heading',
+        textDecoration: 'none',
+        transition: 'color 0.3s linear',
+        display: 'flex',
+        alignItems: 'center',
+      justifyContent: 'center',
+      paddingBottom: '0.1em',
+      wordWrap: 'break-word',
+        '@media (min-width: 830px)': {
+          justifyContent: 'flex-start'
+        }
+      }
+    }}>
+    {children}
+  </nav>
+)
 
-  transition: transform 0.3s ease-in-out;
-  width: 100%;
-
-  @media (min-width: ${(props) => props.theme.sizes.sidebarBreakpoint}px) {
-    width: ${(props) => props.theme.sizes.sidebar}px;
-    top: 0;
-    left: 0;
-  }
-
-  a {
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: ${(props) => props.theme.colors.sidebar.color};
-    font-family: ${(props) => props.theme.fonts.heading};
-    text-decoration: none;
-    transition: color 0.3s linear;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-bottom: 0.1em;
-    word-wrap: break-word;
-
-    @media (min-width: ${(props) => props.theme.sizes.sidebarBreakpoint}px) {
-      justify-content: flex-start;
-    }
-  }
-`
-
-export default StyledMenu
+export default Menu

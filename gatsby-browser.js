0529759fa9ed220cb9245f422781f18b805cfc8f
Sergio C. Orozco Torres <scot3004@gmail.com>
2021-04-03 01:40:10 -0500

    Offline support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,13 @@ import { SidebarProvider } from "./src/context/SidebarContext"
 export const wrapRootElement = ({ element }) => (
   <SidebarProvider>{element}</SidebarProvider>
 )
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(`
+    Hice cambios en este sitio web,
+    ¿deseas recargar para aplicarlos?
+  `)
+  if (answer === true) {
+    window.location.reload()
+  }
+}
